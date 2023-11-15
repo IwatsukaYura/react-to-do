6d59ba40c99e98bef1f943a6475441064c4b4a80
IwatsukaYura <iwatsukayura@gmail.com>
2023-11-15 23:43:22 +0900

    create=react-app

---
+++ src/types/index.ts
@@ -0,0 +1,17 @@
+import { type } from "os";
+
+export type Task = {
+  id: number;
+  title: string;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export type CsrfToken = {
+  csrf_token: string
+}
+
+export type Credential = {
+  email: string;
+  password: string;
+}
